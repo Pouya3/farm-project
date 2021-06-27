fe30c36a684bb166e1a8cc81d2639930746c33fc
Mahdi <mahdiaghajanian@gmail.com>
2021-06-27 12:42:37 +0430

    add setter and getter

--- habitat.cpp
+++ habitat.cpp
@@ -1,6 +1,21 @@
 #include "habitat.h"
 
-Habitat::Habitat()
-{
+Habitat::Habitat() {
 
 }
+bool Habitat::Is_full() {
+
+}
+bool Habitat::Add(int type, int amount) {
+
+}
+bool Habitat::Delete(int type, int amount) {
+
+}
+int  Habitat::Get_time() {
+	return time;
+}
+void Habitat::Set_time(int _time) {
+	time += _time;
+}
+
