b4d267d592d931e880ded866f27e03c76764b500
Mahdi <mahdiaghajanian@gmail.com>
2021-06-27 12:16:59 +0430

    Update habitat.h

--- habitat.h
+++ habitat.h
@@ -1,11 +1,19 @@
 #ifndef HABITAT_H
 #define HABITAT_H
-
-
-class Habitat
-{
+#include "Building.h"
+class Habitat {
+protected:
+	int status;
+	int time;
+	int level;//
 public:
     Habitat();
+	virtual void Feed();
+	virtual void Collect();
+	bool Is_full();
+	bool Add(int type, int amount);//
+	bool Delete(int type, int amount);//
+	int Get_time();
+	void Set_time(int _time);
 };
-
 #endif // HABITAT_H
