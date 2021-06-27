67d6576c3ccbe22edcd5c298908a3610b6d74da3
Mahdi <mahdiaghajanian@gmail.com>
2021-06-27 12:23:57 +0430

    Update aviculture.h

--- aviculture.h
+++ aviculture.h
@@ -1,11 +1,14 @@
 #ifndef AVICULTURE_H
 #define AVICULTURE_H
 
+#include "Habitat.h"
 
-class Aviculture
-{
+class Aviculture :public Habitat {
 public:
     Aviculture();
+	void Upgrade();
+	void Feed();
+	void Collect();
 };
 
 #endif // AVICULTURE_H
