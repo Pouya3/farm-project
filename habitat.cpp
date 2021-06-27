ac7d3d474c289e9718cf2e19d95e367b41f36d5c
Mahdi <mahdiaghajanian@gmail.com>
2021-06-27 16:36:25 +0430

    complete habitat.cpp

--- habitat.cpp
+++ habitat.cpp
@@ -1,16 +1,27 @@
 #include "habitat.h"
 
 Habitat::Habitat() {
-
+    status=0;
+    time=0;
 }
 bool Habitat::Is_full() {
-
+    if(used_sorage==total_storage)
+        return true;
+    return false;
 }
 bool Habitat::Add(int type, int amount) {
-
+    if(used_storage+amount<=total_storage){
+        used_storage+=amount;
+        return true;
+    }
+    return false;
 }
 bool Habitat::Delete(int type, int amount) {
-
+    if(used_storage-amount>=0){
+        used_storage-=amount;
+        return true;
+    }
+    return false;
 }
 int  Habitat::Get_time() {
 	return time;
