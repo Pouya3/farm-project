28e59a7de66e979c2ca0b33056d47d95cb7ac501
alitmz <engineer.at20@gmail.com>
2021-07-02 21:39:15 +0430

    Update wheat_land.cpp

--- wheat_land.cpp
+++ wheat_land.cpp
@@ -1,6 +1,21 @@
 #include "wheat_land.h"
 
-Wheat_land::Wheat_land()
+Wheat_land::Wheat_land(User* _user, Store* _store)
 {
+    store = _store;
+    user = _user;
+    is_active=true;
+    space=5;
+}
 
+void Wheat_land::Upgrade() {
+    if(store->Get_object(1)>=1 && user->Get_coin()>=5 && user->Get_level()>=2) {
+        store->Delete(1, 1);         
+        user->Set_coin(user->Get_coin()-5);
+        user->Set_experience(user->Get_experience()+3);
+        space*=2;
+    }
+    else { // not enough resources for upgrading
+        
+    }
 }
