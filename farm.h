554d1051fdd9ff447ecd73784446be53a80ac599
alitmz <engineer.at20@gmail.com>
2021-07-02 19:59:07 +0430

    Update farm.h

--- farm.h
+++ farm.h
@@ -1,11 +1,23 @@
 #ifndef FARM_H
 #define FARM_H
+#include "wheat_land.h"
+#include "alfalfa_land.h"
+#include "store.h"
+#include "silo.h"
+#include "aviculture.h"
+#include "aghol.h"
+#include "livestock.h"
 
+class Farm {
+private:
+    Wheat_land wheat_land;
+    Alfalfa_land alfalfa_land;
+    Store store;
+    Silo silo;
+    Aviculture aviculture;
+    Aghol aghol;
+    Livestock livestock;
 
-class Farm
-{
-public:
-    Farm();
 };
 
 #endif // FARM_H
