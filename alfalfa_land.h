cdd7c4be69083cd340c4843a40c00234e6c6827a
alitmz <engineer.at20@gmail.com>
2021-07-02 22:04:06 +0430

    Update alfalfa_land.h

--- alfalfa_land.h
+++ alfalfa_land.h
@@ -1,11 +1,19 @@
 #ifndef ALFALFA_LAND_H
 #define ALFALFA_LAND_H
+#include "land.h"
+#include "user.h"
+#include "store.h"
 
+class Alfalfa_land :public Land{
+    
+private:
+    User* user;
+    Store* store;
 
-class Alfalfa_land
-{
 public:
-    Alfalfa_land();
+    Alfalfa_land(User* _user, Store* _store);
+    void Upgrade();
+
 };
 
 #endif // ALFALFA_LAND_H
