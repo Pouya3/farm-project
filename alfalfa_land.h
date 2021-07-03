926c9cb556de7f6e69c915d34dd320ef11a7cad2
alitmz <engineer.at20@gmail.com>
2021-07-03 13:59:55 +0430

    Update alfalfa_land.h

--- alfalfa_land.h
+++ alfalfa_land.h
@@ -1,19 +1,15 @@
 #ifndef ALFALFA_LAND_H
 #define ALFALFA_LAND_H
 #include "land.h"
-#include "user.h"
-#include "store.h"
 
-class Alfalfa_land :public Land{
-    
-private:
-    User* user;
-    Store* store;
-
+// status: 0 zamin bikar(shokm nazade) ... 1 zamin darhale shokm ... 2 shokm be payan reside
+// ... 3 zamin dar hale kesht mahsole asli ... 4 kesht zamin tamam shode va montazere jamavari
+class Alfalfa_land :public Land
+{
 public:
     Alfalfa_land(User* _user, Store* _store);
     void Upgrade();
-
+    void Plow(int);
 };
 
 #endif // ALFALFA_LAND_H
