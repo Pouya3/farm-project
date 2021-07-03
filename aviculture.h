163bd398420633edc94c39f39f7589c90331a585
Mahdi <mahdiaghajanian@gmail.com>
2021-07-03 11:21:21 +0430

    complete aviculture.h

--- aviculture.h
+++ aviculture.h
@@ -2,13 +2,19 @@
 #define AVICULTURE_H
 
 #include "habitat.h"
+#include "silo.h"
+#include "store.h"
 
 class Aviculture :public Habitat {
+    Silo* silo_ptr;
+    Store* store_ptr;
 public:
-    Aviculture();
-	void Upgrade();
-	void Feed();
-	void Collect();
+    Aviculture(Silo* _silo_ptr,Store* _store_ptr);
+    void Upgrade();
+    bool Feed();
+    bool Collect();
+    bool Add(int type, int amount);//
+    bool Delete(int type, int amount);//
 };
 
 #endif // AVICULTURE_H
