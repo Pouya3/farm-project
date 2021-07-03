14bcd4b0f6c0298b5c77fbf1ef2132331083ce57
alitmz <engineer.at20@gmail.com>
2021-07-03 19:30:37 +0430

    Update wheat_land.h

--- wheat_land.h
+++ wheat_land.h
@@ -1,14 +1,22 @@
 #ifndef WHEAT_LAND_H
 #define WHEAT_LAND_H
 #include "land.h"
+#include "silo.h"
 
 // status: 0 zamin bikar ... 1 zamin dar hale kesht ... 2 kesht zamin tamam shode va montazere jamavari
 
 class Wheat_land : public Land 
 {
+
+private:
+    Silo* silo; 
+
 public:
-    Wheat_land(User* _user, Store* _store);
+    Wheat_land(User* _user, Store* _store,Silo* _silo);
+
     void Upgrade();
+    void Cultivation(int);
+    void Harvesting(int);
 };
 
 #endif // WHEAT_LAND_H
