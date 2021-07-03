49cbd9d63a3c99fa8ba6d3e18066d4d04290e87b
Mahdi <mahdiaghajanian@gmail.com>
2021-07-03 10:02:10 +0430

    complete aghol.cpp

--- aghol.cpp
+++ aghol.cpp
@@ -1,6 +1,32 @@
 #include "aghol.h"
 
-Aghol::Aghol()
-{
-
+Aghol::Aghol(Store* _store_ptr){
+    store_ptr=_store_ptr;
+    total_storage=2;
+    used_storage=0;
+    return;
 }
+void Aghol::Upgrade() {
+	level++;
+	total_storage *= 2;
+}
+bool Aghol::Feed() {
+    if(store_ptr->Delete(3, used_storage)){
+    return true;
+    }
+    return false;
+}
+int Aghol::Wool_shaving() {
+    if(user_ptr->Get_coin()>=used_storage){
+        if(store_ptr->Get_total_storage()-store_ptr->Get_used_storage()>=used_storage){
+            //user_ptr->Delete_coin(used_storage);
+            store_ptr->Add(6,used_storage);
+            return 1;// to send wool added
+        }
+        return -1;// to send not enogh space in store
+    }
+    return -2;// to send not enogh coin
+}
+/*bool Aghol::Collect() {
+
+}*/
