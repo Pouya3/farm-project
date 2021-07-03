b6811a2937b8a72426fad0d20903f33e0eff5998
Mahdi <mahdiaghajanian@gmail.com>
2021-07-03 10:01:36 +0430

    complete aghol.h

--- aghol.h
+++ aghol.h
@@ -1,15 +1,17 @@
 #ifndef AGHOL_H
 #define AGHOL_H
 
-#include "Habitat.h"
+#include "habitat.h"
+#include "store.h"
 
 class Aghol :public Habitat {
+    Store* store_ptr;
 public:
-	Aghol();
-	void Upgrade();
-	void Feed();
-	void Collect();
-	void Wool_shaving();
+    Aghol(Store* _store_ptr);
+    void Upgrade();
+	bool Feed();
+	//bool Collect();
+	int Wool_shaving();
 };
 
 #endif // AGHOL_H
