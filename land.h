312082c1a23aa26791346cc41da765da286dba4c
alitmz <engineer.at20@gmail.com>
2021-07-03 13:59:57 +0430

    Update land.h

--- land.h
+++ land.h
@@ -1,6 +1,8 @@
+#pragma once
 #ifndef LAND_H
 #define LAND_H
-
+#include "user.h"
+#include "store.h"
 
 class Land {
 protected:
@@ -9,11 +11,13 @@ protected:
     int space;
     int status;
     bool is_active;
+    User* user;
+    Store* store;
 
 public:
     Land();
 
-    virtual void Upgrade();
+    virtual void Upgrade()=0;
     void Cultivation();
     int Get_time();
     void Set_time(int);
