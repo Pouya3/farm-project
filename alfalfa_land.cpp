8c7a5055929052fd5776cf1c3670d53439451352
alitmz <engineer.at20@gmail.com>
2021-07-03 13:59:53 +0430

    Update alfalfa_land.cpp

--- alfalfa_land.cpp
+++ alfalfa_land.cpp
@@ -4,15 +4,17 @@ Alfalfa_land::Alfalfa_land(User* _user, Store* _store)
 {
     store = _store;
     user = _user;
-    if(user->Get_level()>=)
+    if(user->Get_level()>=3)
         is_active=true;
-    space=5;
+    else
+        is_active=false;
+    space=4;
 }
 
 void Alfalfa_land::Upgrade() 
 {
-    if (store->Get_object(1)>=1 && user->Get_coin()>=5 && user->Get_level()>=2) {
-        store->Delete(1, 1);         
+    if (store->Get_object(1)>=2 && user->Get_coin()>=5 && user->Get_level()>=4) {
+        store->Delete(1, 2);         
         user->Set_coin(user->Get_coin()-5);
 
         // Qtimer
@@ -24,8 +26,30 @@ void Alfalfa_land::Upgrade()
     }
     else { // not enough resources for upgrading
 
-        if(store->Get_object(1)<1) {  } // shovel needed
+        if(store->Get_object(1)<2) {  } // shovel needed
         else if (user->Get_coin()<5) { } // coin needed
         else { } // level needed
+    }
+}
+
+void Alfalfa_land::Plow(int tedad) 
+{
+    if(status==0) { // zamin bikar
+        if(user->Get_coin()>=5*tedad && user->Get_level()>=3){
+            user->Set_coin(user->Get_coin()-5);
+            status=1;
+            // Qtimer
+
+            // after Qtimer finished :
+            status=2;
+            user->Set_experience(user->Get_experience()+tedad);
+        }
+        else { // not enough resources for upgrading
+            if (user->Get_coin()<5*tedad) { } // coin needed
+            else { } // level needed
+        }
+    }
+    else { // zamin ra nemi shavad shokm zad
+
     }
 }
