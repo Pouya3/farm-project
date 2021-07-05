835f522f567c60b87f5e769112d18e628e8adf85
alitmz <engineer.at20@gmail.com>
2021-07-05 10:28:26 +0430

    Update store.h

--- store.h
+++ store.h
@@ -5,6 +5,13 @@
 #include <vector>
 using namespace std;
 
+class Aghol;
+class Alfalfa_land;
+class Aviculture;
+class Livestock;
+class Silo;
+class Wheat_land;
+
 class Store : public Building
 {
     int shovel;
@@ -13,11 +20,19 @@ class Store : public Building
     int egg;
     int milk;
     int wool;
-    User* user;
     vector <pair<int, int>> milk_info;
 
 public:
-    Store(User*);
+    User* user; // this -> user
+
+    Aghol* aghol; // aghol -> this
+    Alfalfa_land* alfalfa_land; // alfalfa_land -> this
+    Aviculture* aviculture; // aviculture -> this
+    Livestock* livestock; // livestock -> this
+    Silo* silo; // silo -> this
+    Wheat_land* wheat_land; // wheat_land -> this
+
+    Store();
     void Upgrade();
     bool Add(int ,int );
     bool Delete(int ,int);
