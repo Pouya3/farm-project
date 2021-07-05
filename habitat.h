28d2541f2cb57515a15de863c414157c2cc2b8aa
alitmz <engineer.at20@gmail.com>
2021-07-05 10:28:53 +0430

    Update habitat.h

--- habitat.h
+++ habitat.h
@@ -1,18 +1,21 @@
 #ifndef HABITAT_H
 #define HABITAT_H
 #include "building.h"
+#include "store.h"
+
 class Habitat: public Building {
 protected:
 	int status;
 	int time;
 	//bool _feed;//
 public:
-    Habitat();
-	virtual bool Feed();
-	virtual bool Collect();
+    Store* store;
+	Habitat();
+    virtual bool Feed() = 0;
+    virtual bool Collect() = 0;
 	bool Is_full();
-	//bool Add(int type, int amount);//
-	//bool Delete(int type, int amount);//
+    //bool Add(int type, int amount);//
+    //bool Delete(int type, int amount);//
 	int Get_time();
 	void Set_time(int _time);
 };
