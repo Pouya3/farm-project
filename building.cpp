59333ea78689b518c2a4023f76def85a39fe9efe
alitmz <engineer.at20@gmail.com>
2021-07-05 10:28:47 +0430

    Update building.cpp

--- building.cpp
+++ building.cpp
@@ -5,3 +5,14 @@ Building::Building()
 
 }
 
+void Building::Set_total_storage(int _total_storage){
+    total_storage = _total_storage;
+}
+
+int Building::Get_total_storage(){
+    return total_storage;
+}
+
+int Building::Get_used_storage(){
+    return used_storage;
+}
