41fcbf6bddd172197c98eaefbab9cdea23e7bb0a
Mahdi <mahdiaghajanian@gmail.com>
2021-07-09 18:59:41 +0430

    Update aghol.h

--- aghol.h
+++ aghol.h
@@ -12,10 +12,10 @@ public:
     Aghol();
     bool Build();
     void Upgrade();
-	bool Feed();
-    //bool Collect();
-	int Wool_shaving();
-	bool Add(int type, int amount);//
+    bool Feed();
+    bool Collect();
+    int Wool_shaving();
+    bool Add(int type, int amount);//
     bool Delete(int type, int amount);//
 
 };
