2ae7e27bcc0d77b5ea9361f7252a9a2830f88c3c
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-10 18:40:43 +0430

    classes and functions refined

--- user.cpp
+++ user.cpp
@@ -16,9 +16,9 @@ User::User() : aghol(NULL), alfalfa_land(NULL), aviculture(NULL), livestock(NULL
     silo->user = this;
     silo->store = this->store;
 
-//    aghol = new Aghol;
-//    aghol->user = this;
-//    aghol->store = this->store;
+    aghol = new Aghol;
+    aghol->user = this;
+    aghol->store = this->store;
 
     alfalfa_land = new Alfalfa_land;
     alfalfa_land->user = this;
@@ -39,6 +39,25 @@ User::User() : aghol(NULL), alfalfa_land(NULL), aviculture(NULL), livestock(NULL
     aviculture->silo = this->silo;
 }
 
+void User::Set_name(QString _name) { name = _name; }
+QString User::Get_name() { return name; }
+void User::set_username(QString _username) { username = _username; }
+QString User::Get_username() { return username; }
+void User::Set_password(QString _password) { password = _password; }
+QString User::Get_password() { return password; }
+void User::Set_email(QString _email) { email = _email; }
+QString User::Get_email() { return email; }
+void User::Set_level(int _level){level=_level;}
+int User::Get_level(){return level;}
+void User::Set_coin(long long int _coin){coin=_coin;}
+int User::Get_experience(){return experience;}
+void User::Set_max_experience(int _experience) { experience = _experience; }
+int User::Get_max_experience() { return experience; }
+void User::Set_time(int _time){time=_time;}
+int User::Get_time(){return time;}
+void User::Set_experience(int _experience){experience=_experience;}
+long long int User::Get_coin(){return coin;}
+
 void User::Check_experience()
 {
     if(experience==max_experience)
@@ -49,12 +68,15 @@ void User::Check_experience()
     }
 }
 
-long long int User::Get_coin(){return coin;}
-int User::Get_level(){return level;}
-int User::Get_time(){return time;}
-int User::Get_experience(){return experience;}
-void User::Set_coin(long long int _coin){coin=_coin;}
-void User::Set_experience(int _experience){experience=_experience;}
-void User::Set_level(int _level){level=_level;}
-void User::Set_time(int _time){time=_time;}
+
+
+
+
+
+
+
+
+
+
+
 
--- user.h
+++ user.h
@@ -31,14 +31,24 @@ public:
     Wheat_land* wheat_land; // wheat_land -> this
 
     User();
-    int Get_level();
-    int Get_experience();
-    long long int Get_coin();
-    int Get_time();
+    void Set_name(QString);//
+    QString Get_name(); //
+    void set_username(QString);//
+    QString Get_username(); //
+    void Set_password(QString);//
+    QString Get_password();//
+    void Set_email(QString);//
+    QString Get_email();//
     void Set_level(int);
+    int Get_level();
     void Set_experience(int);
-    void Set_coin(long long int);
+    int Get_experience();
+    void Set_max_experience(int);//
+    int Get_max_experience(); //
     void Set_time(int);
+    int Get_time();
+    void Set_coin(long long int);
+    long long int Get_coin();
     void Check_experience();
 };
 
