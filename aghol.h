e8398d7db82411fb385f592bb28ff6b7cdd73f27
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-10 11:41:07 +0430

    classes and functions refined

--- aghol.h
+++ aghol.h
@@ -10,20 +10,11 @@ public:
     Store* store; // this -> store
 
     Aghol();
-<<<<<<< HEAD
     int Build();
     int Upgrade();
     int Feed();
     int Collect();
 	bool Add(int type, int amount);//
-=======
-    bool Build();
-    void Upgrade();
-    bool Feed();
-    bool Collect();
-    int Wool_shaving();
-    bool Add(int type, int amount);//
->>>>>>> 41fcbf6bddd172197c98eaefbab9cdea23e7bb0a
     bool Delete(int type, int amount);//
 
 };
