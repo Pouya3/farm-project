54adefe6f8152d44a14a329e838eefc6523a52ed
Mahdi <mahdiaghajanian@gmail.com>
2021-07-11 16:29:00 +0430

    Create time.cpp

---
+++ time.cpp
@@ -0,0 +1,15 @@
+#include "time.h"
+
+void Time :: Set_hour(int _hour){
+  hour=_hour;
+  if(hour==24){
+    hour=0;
+    day++;
+  }
+}
+int Time :: Get_hour(){
+ return hour; 
+}
+int Time :: Get_day(){
+ return day; 
+}
