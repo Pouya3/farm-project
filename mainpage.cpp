941a7ef5ab4c423dc7d3db883145967fddb655eb
Mahdi <mahdiaghajanian@gmail.com>
2021-07-11 16:46:28 +0430

    Update mainpage.cpp

--- mainpage.cpp
+++ mainpage.cpp
@@ -14,42 +14,45 @@ MainPage::MainPage(QWidget *parent) :
 {
     ui->setupUi(this);
     this->setFixedSize(1265,540);
+    timer=new QTimer (this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(Time_function()));
+    timer->start(15000);
 }
 
-MainPage::~MainPage()
+MainPage:: ~MainPage()
 {
     delete ui;
 }
 
-void MainPage::on_pushButton_a_clicked()
+void MainPage:: on_pushButton_a_clicked()
 {
     StorePage* s=new StorePage;
     s->show();
 }
 
 
-void MainPage::on_pushButton_a_8_clicked()
+void MainPage:: on_pushButton_a_8_clicked()
 {
     LivestockPage* l=new LivestockPage;
     l->show();
 }
 
 
-void MainPage::on_pushButton_a_5_clicked()
+void MainPage:: on_pushButton_a_5_clicked()
 {
     Alfalfa_landPage* a=new Alfalfa_landPage;
     a->show();
 }
 
 
-void MainPage::on_pushButton_a_6_clicked()
+void MainPage:: on_pushButton_a_6_clicked()
 {
     Wheat_landPage* w=new Wheat_landPage;
     w->show();
 }
 
 
-void MainPage::on_pushButton_a_7_clicked()
+void MainPage:: on_pushButton_a_7_clicked()
 {
     SiloPage* s=new SiloPage;
     s->show();
@@ -63,16 +66,19 @@ void MainPage::on_pushButton_a_4_clicked()
 }
 
 
-void MainPage::on_pushButton_a_3_clicked()
+void MainPage:: on_pushButton_a_3_clicked()
 {
     AgholPage* a=new AgholPage;
     a->show();
 }
 
 
-void MainPage::on_pushButton_mm_2_clicked()
+void MainPage:: on_pushButton_mm_2_clicked()
 {
     MenuPage* m=new MenuPage;
     m->show();
 }
+void MainPage:: Time_function(){
+    user->Get_time().Set_hour(user->Get_time().Get_hour()+1);
 
+}
