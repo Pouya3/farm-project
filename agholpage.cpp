d5b65db6f2597319d7ec370d26839ef8f19a10e2
Mahdi <mahdiaghajanian@gmail.com>
2021-07-12 22:46:58 +0430

    add messages to aghol

--- agholpage.cpp
+++ agholpage.cpp
@@ -1,6 +1,6 @@
 #include "agholpage.h"
 #include "ui_agholpage.h"
-
+#include <QMessageBox>
 AgholPage::AgholPage(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::AgholPage)
@@ -21,21 +21,28 @@ AgholPage::~AgholPage()
 
 void AgholPage::on_pushButton_clicked()
 {
+
     switch (aghol->Build()){
-    case 1:
+    case 1:{
         //qmessagebox --> "aghol is still locked"
+        QMessageBox::critical(this,"LOCK","Aghol is still locked");
+    }
         break;
     case 2:
         //qmessagebox --> "not enough coins for building aghol"
+        QMessageBox::critical(this,"COIN","Not enough coins for building Aghol");
         break;
     case 3:
         //qmessagebox --> "not enough nails for building aghol"
+        QMessageBox::critical(this,"NAIL","Not enough nails for building Aghol");
         break;
     case 4:
         //qmessagebox --> "not enough shovels for building aghol"
+        QMessageBox::critical(this,"SHOVEL","Not enough shovels for building Aghol");
         break;
     case 5:
         //qmessagebox --> "timer set for building"
+        QMessageBox::information(this,"START","Timer set for building");
         break;
     }
 }
@@ -46,24 +53,31 @@ void AgholPage::on_pushButton_2_clicked()
     switch (aghol->Upgrade()){
     case 1:
         //qmessagebox --> "aghol is not built yet"
+        QMessageBox::critical(this,"BUILT","Aghol is not built yet");
         break;
     case 2:
         //qmessagebox --> "you cannot upgrade until feeding timer finishes and wools get collected"
+        QMessageBox::critical(this,"FEEDING","You cannot upgrade until feeding timer finishes and wools get collected");
         break;
     case 3:
         //qmessagebox --> "you cannot upgrade aghol until user reaches to level 7"
+        QMessageBox::critical(this,"LEVEL","You cannot upgrade aghol until user reaches to level 7");
         break;
     case 4:
         //qmessagebox --> "not enough coins for upgrading"
+        QMessageBox::critical(this,"COIN","Not enough coins for upgrading");
         break;
     case 5:
-        //qmessagebox --> "not enough nails for upgrading
+        //qmessagebox --> "not enough nails for upgrading"
+        QMessageBox::critical(this,"NAIL","Not enough nails for upgrading");
         break;
     case 6:
         //qmessagebox --> "not enough shovels for upgrading"
+        QMessageBox::critical(this,"SHOVEL","Not enough shovels for upgrading");
         break;
     case 7:
         //qmessagebox --> "timer set for upgrading"
+        QMessageBox::information(this,"START","Timer set for upgrading");
         break;
     }
 }
@@ -74,18 +88,23 @@ void AgholPage::on_pushButton_3_clicked()
     switch(aghol->Feed()){
     case 1:
         //qmessagebox --> "aghol is not built yet"
+        QMessageBox::critical(this,"BUILT","Aghol is not built yet");
         break;
     case 2:
         //qmessagebox --> "aghol is empty"
+        QMessageBox::critical(this,"EMPTY","Aghol is empty");
         break;
     case 3:
         //qmessagebox --> "after timer finished and sheep are shaved youn can feed them"
+        QMessageBox::critical(this,"TIMER","After timer finished and sheep are shaved youn can feed them");
         break;
     case 4:
         //qmessagebox --> "not enough alfalfa for feeding"
+        QMessageBox::critical(this,"ALFALFA","Not enough alfalfa for feeding");
         break;
     case 5:
         //qmessagebox --> "timer set for product"
+        QMessageBox::information(this,"START","Timer set for product");
         break;
     }
 }
@@ -96,18 +115,23 @@ void AgholPage::on_pushButton_4_clicked()
     switch(aghol->Collect()){
     case 1:
         //qmessagebox --> "aghol is not built yet"
+        QMessageBox::critical(this,"BUILT","Aghol is not built yet");
         break;
     case 2:
         //qmessagebox --> "no wool to shave"
+        QMessageBox::critical(this,"WOOL","No wool to shave");
         break;
     case 3:
         //qmessagebox --> "not enough coins for shaving"
+        QMessageBox::critical(this,"COIN","Not enough coins for shaving");
         break;
     case 4:
         //qmessagebox --> "not enough space in store for wools"
+        QMessageBox::critical(this,"SPACE","Not enough space in store for wools");
         break;
     case 5:
         //qmessagebox --> "wool shaved successfully"
+        QMessageBox::information(this,"SHAVED","Wool shaved successfully");
         break;
     }
 }
