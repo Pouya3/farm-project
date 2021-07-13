a969560b6c35bd4513c9b5f3a85eef784fd2d421
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-13 09:32:05 +0430

    QMessageBox added to sheepmarket

--- sheepmarket.cpp
+++ sheepmarket.cpp
@@ -21,16 +21,19 @@ void SheepMarket::on_pushButton_clicked()//1 -> buy                 2 -> sell
 {
     if(aghol->Get_building_status() != 2){
         //qmessagebox --> "you must build livestock to buy or sell sheep"
+        QMessageBox::critical(this,"NOT BUILT","you must build livestock to buy or sell sheep");
     }
     else{
         if((ui->spinBox->text().toInt() == 0)&&(ui->spinBox_2->text().toInt() == 0)){
             //qmessagebox --> "number of buying and selling items is 0"
+            QMessageBox::critical(this,"0 VALUE","number of buying and selling items is 0");
             return;
         }
 
         if(ui->spinBox_2->text().toInt() != 0){                                                                     //
             if(aghol->Get_used_storage() < ui->spinBox_2->text().toInt()){                                          //
                 //qmessagebox --> "not enough sheep to sell"                                                        //
+                QMessageBox::critical(this,"NOT ENOUGH SHEEP","not enough sheep to sell");
             }                                                                                                       //
             else{                                                                                                   // sell
                 aghol->Delete(1, ui->spinBox_2->text().toInt());                                                    //
@@ -41,6 +44,7 @@ void SheepMarket::on_pushButton_clicked()//1 -> buy                 2 -> sell
         if(ui->spinBox->text().toInt() != 0){                                                                       //
             if(aghol->Get_total_storage() - aghol->Get_used_storage() < ui->spinBox->text().toInt()){               //
                 //qmessagebox --> "not enough space in aghol"                                                       //
+                QMessageBox::critical(this,"NOT ENOUGH SPACE","not enough space in aghol");
             }                                                                                                       //
             else{                                                                                                   // buy
                 aghol->Add(1, ui->spinBox->text().toInt());                                                         //
