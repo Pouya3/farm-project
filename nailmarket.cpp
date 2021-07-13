ef6a8dcbdb4856c2c6b80dd121362a738a3e097f
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-13 09:27:53 +0430

    QMessageBox added to nailmarket

--- nailmarket.cpp
+++ nailmarket.cpp
@@ -22,22 +22,25 @@ void NailMarket::on_pushButton_clicked()
 
     if((ui->spinBox->text().toInt() == 0)&&(ui->spinBox_2->text().toInt() == 0)){
         // qmessagebox --> "number of buying and selling items is 0"
+        QMessageBox::critical(this,"0 VALUE","number of buying and selling items is 0");
         return;
     }
 
-    if(ui->spinBox->text().toInt() != 0){                                            //
-        if(store->Get_object(2) < ui->spinBox->text().toInt()){                      //
-            //qmessagbox --> "not enough nails to sell"                              //
-        }                                                                            // sell
-        else{                                                                        //
-            store->Delete(2, ui->spinBox->text().toInt());                           //
-            user->Set_coin(user->Get_coin() + ui->spinBox->text().toInt()*20);       //
-        }                                                                            //
-    }                                                                                //
+    if(ui->spinBox->text().toInt() != 0){                                               //
+        if(store->Get_object(2) < ui->spinBox->text().toInt()){                         //
+            //qmessagbox --> "not enough nails to sell"                                 //
+            QMessageBox::critical(this,"NOT ENOUGH NAILS","not enough nails to sell");  //
+        }                                                                               // sell
+        else{                                                                           //
+            store->Delete(2, ui->spinBox->text().toInt());                              //
+            user->Set_coin(user->Get_coin() + ui->spinBox->text().toInt()*20);          //
+        }                                                                               //
+    }                                                                                   //
 
     if(ui->spinBox_2->text().toInt() != 0){ // buy                                                   //
         if(store->Get_total_storage() - store->Get_used_storage() < ui->spinBox_2->text().toInt()){  //
-            //qmessagebox --> "not enough nails"                                                     //
+            //qmessagebox --> "not enough space in store"                                            //
+            QMessageBox::critical(this,"NOT ENOUGH SPACE","not enough space in store");              //
         }                                                                                            //
         else{                                                                                        //
             store->Add(2, ui->spinBox_2->text().toInt());                                            // buy
