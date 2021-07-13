736bab6a6ef2de3fc11159686a106d1c73578960
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-13 08:54:57 +0430

    QMessageBox added to chicken market

--- chickenmarket.cpp
+++ chickenmarket.cpp
@@ -21,16 +21,19 @@ void ChickenMarket::on_pushButton_clicked()
 {
     if(aviculture->Get_building_status() != 2){
         //messagebox --> "to buy chickens you must build aviculture first"
+        QMessageBox::critical(this,"AVICULTURE NOT BUILT","to buy chickens you must build aviculture first");
     }
     else{
         if((ui->spinBox->text().toInt() == 0)&&(ui->spinBox_2->text().toInt() != 0)){
            //qmessagebox --> "number of buying and selling items is 0"
+            QMessageBox::critical(this,"0 VALUE","number of buying and selling items is 0");
             return;
         }
 
         if(ui->spinBox->text().toInt() != 0){                                                                 //
             if(aviculture->Get_used_storage() < ui->spinBox->text().toInt()){                                 //
-                //qmessagebox --> "not enough chickens to sell                                                //
+                //qmessagebox --> "not enough chickens to sell"                                               //
+                QMessageBox::critical(this,"NOT ENOUGH CHICKENS","not enough chickens to sell");              //
             }                                                                                                 //
             else{                                                                                             // sell
                 aviculture->Delete(1, ui->spinBox->text().toInt());                                           //
@@ -40,7 +43,8 @@ void ChickenMarket::on_pushButton_clicked()
 
         if(ui->spinBox_2->text().toInt() != 0){                                                                     //
             if(aviculture->Get_total_storage() - aviculture->Get_used_storage() < ui->spinBox_2->text().toInt()){   //
-                //qmessagebox --> "not enough space inaviculture"                                                   //
+                //qmessagebox --> "not enough space in aviculture"                                                  //
+                QMessageBox::critical(this,"NOT ENOUGH SPACE","not enough space in aviculture");                    //
             }                                                                                                       //
             else{                                                                                                   // buy
                 aviculture->Add(1, ui->spinBox_2->text().toInt());                                                  //
