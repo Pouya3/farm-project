18e678022f4a930ac21a7e396d9cfd4951d3b89b
alitmz <engineer.at20@gmail.com>
2021-07-15 13:15:49 +0430

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QIcon>
+#include <QPixmap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -23,5 +25,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    QIcon* icon;
+    QPixmap* pix;
 };
 #endif // MAINWINDOW_H
