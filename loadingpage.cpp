9ed67ceb282b1ca74857aefcb62d97d6a68f5aa2
Pouya gh <pouyaghani1@gmail.com>
2021-07-16 13:01:21 -0700

    make a nice loading!!!

--- loadingpage.cpp
+++ loadingpage.cpp
@@ -1,6 +1,7 @@
 #include "loadingpage.h"
 #include "ui_loadingpage.h"
 #include "loginpage.h"
+#include <QMovie>
 #include "mainpage.h"
 #include <iostream>
 
@@ -11,6 +12,9 @@ LoadingPage::LoadingPage(User* _user, QWidget *parent) :
 //    int a=0;
     ui->setupUi(this);
     this->setFixedSize(750,450);
+    QMovie* movie=new QMovie("../farm-project/rr.gif");
+    ui->label->setMovie(movie);
+    movie->start();
 /*    if(a==0) {
 
         LoginPage* l=new LoginPage;
@@ -23,7 +27,7 @@ LoadingPage::LoadingPage(User* _user, QWidget *parent) :
 
     timer = new QTimer (this);
     connect(timer,SIGNAL(timeout()),this,SLOT(close_this()));
-    timer->start(1000);
+    timer->start(6000);
 }
 
 void LoadingPage::changing()
