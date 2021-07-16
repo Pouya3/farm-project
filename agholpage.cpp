d40c39232a8ed9a0a638b43261564ce5a49e5e6f
Mahdi <mahdiaghajanian@gmail.com>
2021-07-16 21:38:02 +0430

    Update agholpage.cpp

--- agholpage.cpp
+++ agholpage.cpp
@@ -177,7 +177,7 @@ void AgholPage::Set_values(){
         ui->label_3->setText("Ready to feed");
     }
     else if((aghol->Get_feeding_status() == 0)){
-        ui->label_3->setText("Empty building");
+        ui->label_3->setText("Empty");
     }
     else if((aghol->Get_feeding_status() == 1)&&(aghol->Get_upgrade_timer() == 0)){
         ui->label_3->setText("Wool ready in " + QString::number(aghol->Get_feeding_timer()));
