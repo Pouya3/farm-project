8ae97f7b1247d23b86defb6b91c8715d042fb1fe
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-16 17:11:01 +0430

    qmessagebox added for successful trade

--- alfalfamarket.cpp
+++ alfalfamarket.cpp
@@ -45,11 +45,21 @@ void AlfalfaMarket::on_pushButton_clicked()
             if(store->Get_object(3) < ui->spinBox->text().toInt()){                               //
                 //qmessagbox --> "not enough alfalfa to sell"                                     //
                 QMessageBox::critical(this,"0 NOT ENOUGH ALFLFA","not enough alfalfa to sell");   //
-            }                                                                                     // sell
+            }                                                                                     //
             else{                                                                                 //
                 store->Delete(3, ui->spinBox->text().toInt());                                    //
                 user->Set_coin(user->Get_coin() + ui->spinBox->text().toInt()*4);                 //
                 user->Set_experience(user->Get_experience() + ui->spinBox_2->text().toInt()*6);   //
+                                                                                                  //
+                if(ui->spinBox->text().toInt() == 1){//                                           // sell
+                    //qmessagebox --> "alfalfa is sold successfully"                              //
+                    QMessageBox::information(this,"SUCCESSFUL TRADE", "1 alfalfa is sold");       //
+                }                                                                                 //
+                else{                                                                             //
+                    //qmessagebox --> "alfalfa is sold successfully"                              //
+                    QMessageBox::information(this,"SUCCESSFUL TRADE",                             //
+                    QString::number(ui->spinBox->text().toInt()) + " alfalfas are sold");         //
+                }                                                                                 //
             }                                                                                     //
         }                                                                                         //
 
@@ -68,9 +78,19 @@ void AlfalfaMarket::on_pushButton_clicked()
                     store->Add(3, ui->spinBox_2->text().toInt());                                                   //
                     user->Set_coin(user->Get_coin() - ui->spinBox_2->text().toInt()*6);                             //
                     user->Set_experience(user->Get_experience() + ui->spinBox_2->text().toInt()*2);                 //
-                }
-            }
-        }
+                                                                                                                    //
+                    if(ui->spinBox_2->text().toInt() == 1){//                                                       //
+                        //qmessagebox --> "alfalfa is bought successfully"                                          //
+                        QMessageBox::information(this,"SUCCESSFUL TRADE", "1 alfalfa is bought");                   //
+                    }                                                                                               //
+                    else{                                                                                           //
+                        //qmessagebox --> "alfalfa is bought successfully"                                          //
+                        QMessageBox::information(this,"SUCCESSFUL TRADE",                                           //
+                        QString::number(ui->spinBox_2->text().toInt()) + " alfalfas are bought");                   //
+                    }                                                                                               //
+                }                                                                                                   //
+            }                                                                                                       //
+        }                                                                                                           //
     }
 }
 
