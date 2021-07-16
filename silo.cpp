d28ab6d472f30ca36cabf25ddd9915b7cf80e957
Matin-Gholipoor <matin.3108@gmail.com>
2021-07-16 21:48:54 +0430

    shovel if added to silo upgrade function

--- silo.cpp
+++ silo.cpp
@@ -11,28 +11,35 @@ Silo::Silo() : wheat_land(NULL) , aviculture(NULL)
 int Silo::Upgrade(){
     // return values :
     // 1 == not enough nails
-    // 2 == not enough coins
-    // 3 == limit for upgrading duo to user's level
-    // 4 == timer set for upgrade (successful upgrade)
+    // 2 == not enough shovels
+    // 3 == not enough coins
+    // 4 == limit for upgrading duo to user's level
+    // 5 == timer set for upgrade (successful upgrade)
 
     if(store->Get_object(2) < this->level*2){ // not enough nails
         return 1;
     }
-    else if(user->Get_coin() < pow(this->level*2, 2)*100){ // not enough coins
+    else if (store->Get_object(1) < level - 2){
         return 2;
+    }
+    else if(user->Get_coin() < pow(this->level*2, 2)*100){ // not enough coins
+        return 3;
     }
     else{ // enough resources for upgrading
         if(user->Get_level() <= this->level+1){ // user's level will be lower than store's level after upgarde store
-            return 3;
+            return 4;
         }
         else{ // no problem for upgrading
             store->Delete(2, this->level*2);
+            if(level != 1){
+                store->Delete(1, level - 2);
+            }
             user->Set_coin(user->Get_coin()-pow(this->level*2, 2)*100);
 
             upgrade_timer = 4;
 
 
-            return 4;
+            return 5;
         }
     }
 }
--- silopage.cpp
+++ silopage.cpp
@@ -27,21 +27,37 @@ SiloPage::~SiloPage()
 
 void SiloPage::on_pushButton_3_clicked()
 {
+    // return values :
+    // 1 == not enough nails
+    // 2 == not enough shovels
+    // 3 == not enough coins
+    // 4 == limit for upgrading duo to user's level
+    // 5 == timer set for upgrade (successful upgrade)
+
     switch(silo->Upgrade()){
     case 1:
         //qmessagebox --> "not enough nails for upgrading"
         QMessageBox::critical(this,"NAILS","not enough nails for upgrading\n " + QString::number(silo->Get_level()*2) + " nails are needed");
         break;
     case 2:
+        //qmessagebox --> "not enough shovels for upgrading"
+        if(silo->Get_level() == 3){
+            QMessageBox::critical(this,"SHOVELS","not enough shovels for upgrading\n1 shovel is needed");
+        }
+        else {
+            QMessageBox::critical(this,"SHOVELS","not enough shovels for upgrading\n " + QString::number(silo->Get_level() - 2) + " shovels are needed");
+        }
+        break;
+    case 3:
         //qmessagebox --> "not enough coins for upgrading"
         QMessageBox::critical(this,"COINS","not enough coins for upgrading \n" +
         QString::number(pow(silo->Get_level()*2, 2)*100) + " coins are needed");
         break;
-    case 3:
-        //qmessagebox --> "you cannot upgrade silo duo to user's level"
-        QMessageBox::critical(this,"LEVEL","you cannot upgrade silo duo to user's level");
-        break;
     case 4:
+        //qmessagebox --> "you cannot upgrade silo duo to user's level"
+        QMessageBox::critical(this,"LEVEL","you cannot upgrade silo duo to user's level \nsilo's level must be one unit less than user's level");
+        break;
+    case 5:
         //qmessagbox --> "timer set for upgrading"
         QMessageBox::information(this,"START","timer set for upgrading");
         break;
